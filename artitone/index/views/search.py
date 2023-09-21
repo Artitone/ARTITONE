f0378811af54d9e4a9f7c59f3b5d59a82a57a396
EdenWuyifan <yfw215@nyu.edu>
2023-09-21 19:06:52 -0400

    add check in search

--- artitone/index/views/search.py
+++ artitone/index/views/search.py
@@ -6,15 +6,17 @@ from artworks.utils.color_meter import filter_by_color_pallate
 
 
 class Filter:
-    def __init__(self, category=None, color=None, keyword=None):
+    def __init__(self, category=None, color=None, texture=None, keyword=None):
         self.category = category
         self.color = color
+        self.texture = texture
         self.keyword = keyword
 
     def gen_dict(self):
         return {
             "category": self.category,
             "color": self.color,
+            "texture": self.texture,
             "keyword": self.keyword,
         }
 
@@ -24,6 +26,8 @@ class Filter:
             artworks = filter_by_category(self.category, artworks)
         if self.color is not None:
             artworks = filter_by_color_pallate(self.color, artworks)
+        if self.texture is not None:
+            artworks = filter_by_texture(self.texture, artworks)
         if self.keyword is not None:
             artworks = artworks.filter(
                 Q(title__icontains=self.keyword)
@@ -33,14 +37,8 @@ class Filter:
         return artworks
 
 
-def filter_by_category(category_name, artworks):
-    try:
-        category = Category.objects.get(name=category_name)
-    except Category.DoesNotExist:
-        category = None
-    if category:
-        artworks = artworks.filter(category=category)
-    return artworks
+def filter_by_category(category, artworks):
+    return artworks.filter(category=category)
 
 
 def parse_search_filter(post):
@@ -53,17 +51,42 @@ def parse_search_filter(post):
     category = post.get("category")
     color = post.get("color")
     keyword = post.get("keyword")
+    texture = post.get("texture")
     filter = Filter(
         category=category_is_valid(category),
         color=color_is_valid(color),
+        texture=texture_is_valid(texture),
         keyword=keyword,
     )
     return filter
 
 
 def category_is_valid(category):
+    try:
+        category = Category.objects.get(name=category)
+    except Category.DoesNotExist:
+        category = None
     return category
 
 
 def color_is_valid(color):
+    if color not in [
+        "modern",
+        "scandinavian",
+        "minimalist",
+        "bohemian",
+        "industrial",
+        "contemporary",
+    ]:
+        return None
     return color
+
+
+def texture_is_valid(texture):
+    if texture not in [Artwork.SATIN, Artwork.MATTE, Artwork.ROUGH]:
+        return None
+    return texture
+
+
+def filter_by_texture(texture, artworks):
+    return artworks.filter(texture=texture)
