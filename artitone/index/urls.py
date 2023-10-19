99e956727a224f65680d015636b77ae33898c53f
qndgsa <qndgsa@gmail.com>
2023-10-19 18:39:32 -0400

    about done

--- artitone/index/urls.py
+++ artitone/index/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from index.views.home import home
+from index.views.about import about
 
 urlpatterns = [
     path("", home, name="home"),
+    path("/about", about, name="about"),
 ]
